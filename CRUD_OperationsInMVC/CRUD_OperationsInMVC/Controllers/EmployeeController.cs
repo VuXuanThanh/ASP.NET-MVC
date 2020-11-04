db45ea1956916613b97b76a372febba4685b5f17
VuXuanThanh <vuxuanthanh2k@gmail.com>
2020-11-04 09:50:36 +0700

    CRUD operation in ASP.NET MVC

---
+++ CRUD_OperationsInMVC/CRUD_OperationsInMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using CRUD_OperationsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeDBContext employeeDB = new EmployeeDBContext();
+            List<Employee> emList = employeeDB.Employees.ToList();
+            return View(emList);
+        }
+    }
+}
